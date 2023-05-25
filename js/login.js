236fea020ca25b2722c71c12170eb89a45fd5693
shakil-py <shakilhaquepy@gmail.com>
2023-05-25 13:23:55 +0600

    add

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ document.getElementById("login-btn").addEventListener("click", function () {
     const email = get_value_by_inputfield("email-field")
     const password = get_value_by_inputfield("password-field")
     if (email === "shakil@gmail.com" && password === "12345") {
-        window.location.href = "bank.html"
+        window.location.href = "https://shakil-py.github.io/bap-er-bank-chalange/bank.html"
     }
 })
