5331a5a1b62feede1e27b77008d257082b7673ee
shakil-py <shakilhaquepy@gmail.com>
2023-05-25 13:17:22 +0600

    add chlange

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ document.getElementById("login-btn").addEventListener("click", function () {
     const email = get_value_by_inputfield("email-field")
     const password = get_value_by_inputfield("password-field")
     if (email === "shakil@gmail.com" && password === "12345") {
-        window.location.href = "index.html"
+        window.location.href = "bank.html"
     }
 })
